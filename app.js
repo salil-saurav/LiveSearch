9daa16470cb5de1dfb2189b7aada93c46b1b7a3e
salil-saurav <salilsaurav.work@gmail.com>
2023-09-23 18:01:32 +0530

    Files modified

--- app.js
+++ app.js
@@ -6,101 +6,108 @@ const body = document.body;
 // const loadMoreButton = document.querySelectorAll(".load-more");
 
 body.addEventListener("click", () => {
-  selectContainer.classList.add("d-none");
-  selectContainer.classList.remove("d-flex");
+    selectContainer.classList.add("d-none");
+    selectContainer.classList.remove("d-flex");
 });
 
 // Adding all options to select element with all avaialble categories
 
 function populateSelectOptions() {
-  categoryDivs.forEach((div) => {
-    const category = div.querySelector("h1").innerText;
-    const option = document.createElement("option");
-    option.value = category;
-    option.textContent = category;
-    categorySelect.appendChild(option);
+    categoryDivs.forEach((div) => {
+        const category = div.querySelector("h1").innerText.trim();
+        const option = document.createElement("option");
+        option.value = category;
+        option.textContent = category;
+        categorySelect.appendChild(option);
 
-    categoryInput.addEventListener("input", (evt) => {
-      let inputValue = evt.target.value;
-      if (inputValue === option.value) {
-        option.selected = inputValue;
-      }
-    });
+        categoryInput.addEventListener("input", (evt) => {
+            let inputValue = evt.target.value;
+            if (inputValue === option.value) {
+                option.selected = inputValue;
+            }
+        });
 
-    option.addEventListener("click", (evt) => {
-      let newValue = evt.target.value;
-      categoryInput.value = newValue;
+        option.addEventListener("click", (evt) => {
+            let newValue = evt.target.value;
+            categoryInput.value = newValue;
+        });
     });
-  });
 }
 
 populateSelectOptions(); // options Added
 
 const selectedOptions = Array.from(categorySelect.options).map(
-  (option) => option.value
+    (option) => option.value
 );
 
 const loweredSelectedOptions = [
-  ...selectedOptions.map((option) => option.toLowerCase()),
+    ...selectedOptions.map((option) => option.toLowerCase()),
+
 ];
 
 const filtered = loweredSelectedOptions.includes(categoryInput.value);
-console.log(filtered);
 
+categoryInput.addEventListener("keyup", (evt) => {
+    if (!filtered && evt.key === "Enter") {
+        console.log("not found");
+    } else {
+        console.log(filtered);
+    }
+});
 // showing the select container when categoryInput value changes
 
 const categoryOptions = Array.from(categorySelect.options);
 
 categoryInput.addEventListener("input", () => {
-  selectContainer.classList.remove("d-none");
-  selectContainer.classList.add("d-flex");
+    selectContainer.classList.remove("d-none");
+    selectContainer.classList.add("d-flex");
 
-  //  displaying all div when input value is falsy
-  if (!categoryInput.value) {
-    categoryDivs.forEach((div) => div.classList.remove("d-none"));
-  }
+    //  displaying all div when input value is falsy
+    if (!categoryInput.value) {
+        categoryDivs.forEach((div) => div.classList.remove("d-none"));
+    }
 
-  const searchText = categoryInput.value.trim().toLowerCase();
+    const searchText = categoryInput.value.trim().toLowerCase();
 
-  categorySelect.innerHTML = "";
+    categorySelect.innerHTML = "";
 
-  // Filter the original options based on the search text
-  const filteredOptions = categoryOptions.filter((option) =>
-    option.textContent.toLowerCase().includes(searchText)
-  );
+    // Filter the original options based on the search text
+    const filteredOptions = categoryOptions.filter((option) =>
+        option.textContent.toLowerCase().includes(searchText)
+    );
 
-  // Add the filtered options back to the select element
-  filteredOptions.forEach((option) => {
-    categorySelect.appendChild(option.cloneNode(true));
-  });
+    // Add the filtered options back to the select element
+    filteredOptions.forEach((option) => {
+        categorySelect.appendChild(option.cloneNode(true));
+    });
 });
 
 //
 
 // Event listener for select change
 categorySelect.addEventListener("change", function () {
-  const selectedCategory = this.value;
+    const selectedCategory = this.value;
 
-  // Hide all category divs
-  categoryDivs.forEach((div) => {
-    div.classList.add("d-none");
-  });
-
-  // Show the selected category div
-  if (selectedCategory) {
+    // Hide all category divs
     categoryDivs.forEach((div) => {
-      const category = div.querySelector("h1").innerText;
-
-      if (selectedCategory === category) {
-        div.classList.remove("d-none");
-        categoryInput.value = selectedCategory;
-      }
+        div.classList.add("d-none");
     });
-  }
+
+    // Show the selected category div
+    if (selectedCategory) {
+        categoryDivs.forEach((div) => {
+            const category = div.querySelector("h1").innerText;
+
+            if (selectedCategory === category) {
+                div.classList.remove("d-none");
+                categoryInput.value = selectedCategory;
+            }
+        });
+    }
 });
 
 categoryDivs.forEach((div, index) => {
-  if (index >= 3) {
-    div.classList.add("d-none");
-  }
+    if (index >= 3) {
+        div.classList.add("d-none");
+    }
 });
